494210fd4886771f6cba93179cb353118ee7b9a7
pavanvish46-tech <pavanvish46@gmail.com>
2025-12-24 19:09:27 +0530

    Complete removal of PostgreSQL dependencies

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'qZikJy06ETFgk7siAfgK_Z9PwjTRdSGKBI4i2e8yZ-U')
     
     # Use SQLite for both local and Vercel
     if os.environ.get('VERCEL'):
